4919753f64f2dbecf25c22caff2f9aaae9bf5d91
Yashodhar Patel <patelyashodhar012@gmail.com>
2021-06-17 10:11:25 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-// Node server which will handle socket io connections
+const express = require('express');
+const app = express();
+const favicon = require('serve-favicon');
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const port = process.env.port || 8000;
-const io = require('socket.io')(port);
- 
 const users = {};
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+// SERVE FAVICON
+app.use(favicon(path.join(__dirname, 'src', 'icon.ico')));
+
+// ENDPOINTS
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/static/CSS/style.css', (req, res) => {
+    res.sendFile(__dirname + "/" + "/static/CSS/style.css");
+});
+
+app.get('/static/JavaScript/client.js', (req, res) => {
+    res.sendFile(__dirname + "/" + "/static/JavaScript/client.js");
+});
+
+app.get('/src/logo.svg', (req, res) => {
+    res.sendFile(__dirname + "/" + "/src/logo.svg");
+});
+
+app.get('/src/send.svg', (req, res) => {
+    res.sendFile(__dirname + "/" + "/src/send.svg");
+});
+
+app.get('/src/ting.mp3', (req, res) => {
+    res.sendFile(__dirname + "/" + "/src/ting.mp3");
+});
 
 io.on('connection', socket =>{
     // If any new user joins, let other users connected to the server know!
@@ -21,4 +58,9 @@ io.on('connection', socket =>{
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
+});
+
+// START THE SERVER
+server.listen(port, ()=> {
+    console.log(`The Application is running successfully on port ${port}`);
 });
