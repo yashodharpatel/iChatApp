71a6b907a20bb531be112f4d501f8e4348cb2215
Yashodhar Patel <patelyashodhar012@gmail.com>
2021-06-18 09:12:07 +0530

    Initial Commit

--- static/JavaScript/client.js
+++ static/JavaScript/client.js
@@ -15,6 +15,7 @@ const append = (message, position) => {
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
     if(position == 'left' || position == 'centre'){
         audio.play();
     }
